24d66c4f4faac2c84efe1bc59dca2d26c3a9ee5a
MohammadAliHasanLafta <1380lafta@gmail.com>
2024-01-05 15:07:56 -0800

    Charts added

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -5,18 +5,18 @@
         class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
       >
         <chart-card
-          :chart-data="dailySalesChart.data"
-          :chart-options="dailySalesChart.options"
+          :chart-data="dailyTempChart.data"
+          :chart-options="dailyTempChart.options"
           :chart-type="'Line'"
           data-background-color="blue"
         >
           <template slot="content">
-            <h4 class="title">Daily Sales</h4>
+            <h4 class="title">Temperature</h4>
             <p class="category">
               <span class="text-success"
-                ><i class="fas fa-long-arrow-alt-up"></i> 55%
+                ><i class="fas fa-long-arrow-alt-down"></i> 10%
               </span>
-              increase in today sales.
+              decrease in today temperature.
             </p>
           </template>
 
@@ -31,22 +31,26 @@
       <div
         class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
       >
-        <chart-card
-          :chart-data="emailsSubscriptionChart.data"
-          :chart-options="emailsSubscriptionChart.options"
-          :chart-responsive-options="emailsSubscriptionChart.responsiveOptions"
-          :chart-type="'Bar'"
+      <chart-card
+          :chart-data="dailyHumChart.data"
+          :chart-options="dailyHumChart.options"
+          :chart-type="'Line'"
           data-background-color="red"
         >
           <template slot="content">
-            <h4 class="title">Email Subscription</h4>
-            <p class="category">Last Campaign Performance</p>
+            <h4 class="title">Humidity</h4>
+            <p class="category">
+              <span class="text-success"
+                ><i class="fas fa-long-arrow-alt-up"></i> 9%
+              </span>
+              increase in today temperature.
+            </p>
           </template>
 
           <template slot="footer">
             <div class="stats">
               <md-icon>access_time</md-icon>
-              updated 10 days ago
+              updated 4 minutes ago
             </div>
           </template>
         </chart-card>
@@ -54,21 +58,26 @@
       <div
         class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
       >
-        <chart-card
-          :chart-data="dataCompletedTasksChart.data"
-          :chart-options="dataCompletedTasksChart.options"
+      <chart-card
+          :chart-data="dailySoilChart.data"
+          :chart-options="dailySoilChart.options"
           :chart-type="'Line'"
           data-background-color="green"
         >
           <template slot="content">
-            <h4 class="title">Completed Tasks</h4>
-            <p class="category">Last Campaign Performance</p>
+            <h4 class="title">Soil Moisture</h4>
+            <p class="category">
+              <span class="text-success"
+                ><i class="fas fa-long-arrow-alt-up"></i> 9%
+              </span>
+              increase in today temperature.
+            </p>
           </template>
 
           <template slot="footer">
             <div class="stats">
               <md-icon>access_time</md-icon>
-              campaign sent 26 minutes ago
+              updated 4 minutes ago
             </div>
           </template>
         </chart-card>
@@ -81,7 +90,7 @@
             <md-icon>thermostat</md-icon>
             <div class="card">
               <div class="rating">
-                <h2><span class="counter">{{ thingspeak.field1 }}</span><sub>°C</sub></h2>
+                <h2><span class="counter">{{ thingspeak[7].field1 }}</span><sub>°C</sub></h2>
                 <div class="block_temp" v-for="i in numbers" v-bind:key="getItemStyle(i)" :style="getItemStyle(i)"></div>
               </div>
             </div>
@@ -111,7 +120,7 @@
             <md-icon>opacity</md-icon>
             <div class="card">
               <div class="rating">
-                <h2><span class="counter">{{ thingspeak.field2 }}</span><sub>%</sub></h2>
+                <h2><span class="counter">{{ thingspeak[7].field2 }}</span><sub>%</sub></h2>
                 <div class="block_hum" v-for="i in numbers" v-bind:key="getItemStyle(i)" :style="getItemStyle(i)"></div>
               </div>
             </div>
@@ -138,7 +147,7 @@
             <md-icon>eco</md-icon>
             <div class="card">
               <div class="rating">
-                <h2><span class="counter">{{ thingspeak.field3 }}</span></h2>
+                <h2><span class="counter">{{ thingspeak[7].field3 }}</span></h2>
                 <div class="block_eco" v-for="i in numbers" v-bind:key="getItemStyle(i)" :style="getItemStyle(i)"></div>
               </div>
             </div>
@@ -244,10 +253,10 @@ export default {
     return {
       thingspeak: [],
       numbers: [],
-      dailySalesChart: {
+      dailyTempChart: {
         data: {
           labels: ["M", "T", "W", "T", "F", "S", "S"],
-          series: [[12, 17, 7, 17, 23, 18, 38]],
+          series: [[]],
         },
         options: {
           lineSmooth: this.$Chartist.Interpolation.cardinal({
@@ -263,18 +272,17 @@ export default {
           },
         },
       },
-      dataCompletedTasksChart: {
+      dailySoilChart: {
         data: {
-          labels: ["12am", "3pm", "6pm", "9pm", "12pm", "3am", "6am", "9am"],
-          series: [[230, 750, 450, 300, 280, 240, 200, 190]],
+          labels: ["M", "T", "W", "T", "F", "S", "S"],
+          series: [[]],
         },
-
         options: {
           lineSmooth: this.$Chartist.Interpolation.cardinal({
             tension: 0,
           }),
           low: 0,
-          high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+          high: 800, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
           chartPadding: {
             top: 0,
             right: 0,
@@ -283,63 +291,45 @@ export default {
           },
         },
       },
-      emailsSubscriptionChart: {
+      dailyHumChart: {
         data: {
-          labels: [
-            "Ja",
-            "Fe",
-            "Ma",
-            "Ap",
-            "Mai",
-            "Ju",
-            "Jul",
-            "Au",
-            "Se",
-            "Oc",
-            "No",
-            "De",
-          ],
-          series: [
-            [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
-          ],
+          labels: ["M", "T", "W", "T", "F", "S", "S"],
+          series: [[]],
         },
         options: {
-          axisX: {
-            showGrid: false,
-          },
+          lineSmooth: this.$Chartist.Interpolation.cardinal({
+            tension: 0,
+          }),
           low: 0,
-          high: 1000,
+          high: 100, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
           chartPadding: {
             top: 0,
-            right: 5,
+            right: 0,
             bottom: 0,
             left: 0,
           },
         },
-        responsiveOptions: [
-          [
-            "screen and (max-width: 640px)",
-            {
-              seriesBarDistance: 5,
-              axisX: {
-                labelInterpolationFnc: function (value) {
-                  return value[0];
-                },
-              },
-            },
-          ],
-        ],
       },
     };
   },
   mounted() {
   },
+  created() {
+    this.fetchData();
+    this.numbers = Array.from(Array(51).keys());
+  },
   methods: {
     fetchData() {
-      const url = 'https://api.thingspeak.com/channels/2389978/feeds.json?api_key=7F5ZOP8I2C34NSN9&results=2';
+      const url = 'https://api.thingspeak.com/channels/2389978/feeds.json?api_key=7F5ZOP8I2C34NSN9&results=8';
       axios.get(url)
         .then(response => {
-          this.thingspeak = response.data.feeds[1];
+          this.thingspeak = response.data.feeds;
+          // this.dailyTempChart.data.series.pop();
+          for (var i = 1; i < this.thingspeak.length+2; i++) {
+            this.dailyTempChart.data.series[0].push(parseInt(this.thingspeak[i].field1));
+            this.dailyHumChart.data.series[0].push(parseInt(this.thingspeak[i].field2));
+            this.dailySoilChart.data.series[0].push(parseInt(this.thingspeak[i].field3));
+          }
         })
         .catch(error => {
           console.error(error);
@@ -351,10 +341,6 @@ export default {
         animationDelay: `${event/15}s`
       }
     },
-  },
-  created() {
-    this.fetchData();
-    this.numbers = Array.from(Array(51).keys());
   },
 };
 </script>
