c6e2725f735d11e3e927cc0fd7dabd976a843c34
MohammadAliHasanLafta <1380lafta@gmail.com>
2024-01-08 02:22:20 -0800

    Dynamic circle bar added

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="content">
+  <div>
+    <div class="content">
     <div class="md-layout">
       <div
         class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
@@ -88,7 +89,8 @@
             <div class="card">
               <div class="rating">
                 <h2><span class="counter">{{ circleTemp }}</span><sub>°C</sub></h2>
-                <div class="block_temp" v-for="i in numbers" v-bind:key="getItemStyle(i)" :style="getItemStyle(i)"></div>
+                <div class="block_temp" v-for="i in numbers" v-bind:key="getItemStyleTemp(i)" :style="getItemStyleTemp(i)">
+                </div>
               </div>
             </div>
           </template>
@@ -118,7 +120,7 @@
             <div class="card">
               <div class="rating">
                 <h2><span class="counter">{{ circleHum }}</span><sub>%</sub></h2>
-                <div class="block_hum" v-for="i in numbers" v-bind:key="getItemStyle(i)" :style="getItemStyle(i)"></div>
+                <div class="block_hum" v-for="i in numbers" v-bind:key="getItemStyleHum(i)" :style="getItemStyleHum(i)"></div>
               </div>
             </div>
           </template>
@@ -145,7 +147,7 @@
             <div class="card">
               <div class="rating">
                 <h2><span :class="getdegTemp">{{ circleSoil }}</span><sub>%</sub></h2>
-                <div class="block_eco" v-for="i in numbers" v-bind:key="getItemStyle(i)" :style="getItemStyle(i)">
+                <div class="block_soil" v-for="i in numbers" v-bind:key="getItemStyleSoil(i)" :style="getItemStyleSoil(i)">
                 </div>
               </div>
             </div>
@@ -172,7 +174,7 @@
             <md-icon>local_florist</md-icon>
             <div class="card">
               <div class="rating">
-                <h2><span class="count">67</span><sub>Days</sub></h2>
+                <h2><span class="count">{{ dateDifference }}</span><sub>Days</sub></h2>
               </div>
             </div>
           </template>
@@ -185,7 +187,7 @@
           <template slot="footer">
             <div class="stats">
               <md-icon>update</md-icon>
-              2023/10/20
+              {{ startDate }}
             </div>
           </template>
         </stats-card>
@@ -210,22 +212,23 @@
           <template slot="content">
             <span class="md-nav-tabs-title">Tasks:</span>
             <md-tabs class="md-success" md-alignment="left">
-              <md-tab id="tab-home" md-label="Bugs" md-icon="bug_report">
+              <md-tab id="tab-home" md-label="Tasks" md-icon="add_task">
                 <nav-tabs-table></nav-tabs-table>
               </md-tab>
 
-              <md-tab id="tab-pages" md-label="Website" md-icon="code">
+              <!-- <md-tab id="tab-pages" md-label="Website" md-icon="code">
                 <nav-tabs-table></nav-tabs-table>
               </md-tab>
 
               <md-tab id="tab-posts" md-label="server" md-icon="cloud">
                 <nav-tabs-table></nav-tabs-table>
-              </md-tab>
+              </md-tab> -->
             </md-tabs>
           </template>
         </nav-tabs-card>
       </div>
     </div>
+  </div>
   </div>
 </template>
 
@@ -249,6 +252,9 @@ export default {
   },
   data() {
     return {
+      startDate: '2023-12-28',
+      endDate: '',
+      dateDifference: '',
       thingspeak: [],
       numbers: [],
       circleTemp: "",
@@ -263,8 +269,8 @@ export default {
           lineSmooth: this.$Chartist.Interpolation.cardinal({
             tension: 0,
           }),
-          low: 0,
-          high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+          low: 20,
+          high: 30, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
           chartPadding: {
             top: 0,
             right: 0,
@@ -301,8 +307,8 @@ export default {
           lineSmooth: this.$Chartist.Interpolation.cardinal({
             tension: 0,
           }),
-          low: 0,
-          high: 100, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+          low: 20,
+          high: 60, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
           chartPadding: {
             top: 0,
             right: 0,
@@ -323,7 +329,7 @@ export default {
   },
   methods: {
     fetchData() {
-      const url = 'https://api.thingspeak.com/channels/2389978/feeds.json?api_key=7F5ZOP8I2C34NSN9&results=14';
+      const url = 'https://api.thingspeak.com/channels/2389978/feeds.json?api_key=7F5ZOP8I2C34NSN9&results=8';
       axios.get(url)
         .then(response => {
           this.thingspeak = response.data.feeds;
@@ -331,25 +337,76 @@ export default {
           this.circleTemp = this.thingspeak[7].field1.slice(0,4);
           this.circleHum = this.thingspeak[7].field2.slice(0,4);
           this.circleSoil = parseInt((parseInt(this.thingspeak[7].field3))*100/1023);          
-
-          for (var i = 1; i < this.thingspeak.length+2; i++) {
-            this.dailyTempChart.data.series[0].push(parseInt(this.thingspeak[2*i+1].field1));
-            this.dailyTempChart.data.labels.push(this.thingspeak[2*i+1].created_at.slice(12,16));
-            this.dailyHumChart.data.series[0].push(parseInt(this.thingspeak[2*i+1].field2));
-            this.dailyHumChart.data.labels.push(this.thingspeak[2*i+1].created_at.slice(12,16));
-            this.dailySoilChart.data.series[0].push(parseInt(this.thingspeak[2*i+1].field3));
-            this.dailySoilChart.data.labels.push(this.thingspeak[2*i+1].created_at.slice(12,16));
+          this.endDate = this.thingspeak[7].created_at.slice(0,10);
+          this.calculateDateDifference()
+          
+          for (var i = 0; i < this.thingspeak.length+1; i++) {
+            this.dailyTempChart.data.series[0].push(parseInt(this.thingspeak[i].field1));
+            this.dailyTempChart.data.labels.push(this.thingspeak[i].created_at.slice(11,16));
+            this.dailyHumChart.data.series[0].push(parseInt(this.thingspeak[i].field2));
+            this.dailyHumChart.data.labels.push(this.thingspeak[i].created_at.slice(11,16));
+            this.dailySoilChart.data.series[0].push(parseInt(this.thingspeak[i].field3));
+            this.dailySoilChart.data.labels.push(this.thingspeak[i].created_at.slice(11,16));
           }
         })
         .catch(error => {
           console.error(error);
         });
     },
-    getItemStyle(event){
-      return {
-        transform: `rotate(${7.2*event}deg)`,
-        animationDelay: `${event/15}s`
+    getItemStyleTemp(event){
+      let circlechild = Math.floor(((parseInt(this.circleTemp))*50)/100);;
+      if (event <= circlechild) {
+          return {
+          transform: `rotate(${7.2*event}deg)`,
+          animationDelay: `${event/15}s`,
+          background: `rgb(255, 16, 16)`,
+        }
       }
+      else {
+        return {
+          transform: `rotate(${7.2*event}deg)`,
+          animationDelay: `${event/15}s`,
+        }
+      }
+    },
+    getItemStyleHum(event){
+      let circlechild = Math.floor(((parseInt(this.circleHum))*50)/100);;
+      if (event <= circlechild) {
+          return {
+          transform: `rotate(${7.2*event}deg)`,
+          animationDelay: `${event/15}s`,
+          background: `rgb(0, 255, 251)`,
+        }
+      }
+      else {
+        return {
+          transform: `rotate(${7.2*event}deg)`,
+          animationDelay: `${event/15}s`,
+        }
+      }
+    },
+    getItemStyleSoil(event){
+      let circlechild = Math.floor(((parseInt(this.circleSoil))*50)/100);;
+      if (event <= circlechild) {
+          return {
+          transform: `rotate(${7.2*event}deg)`,
+          animationDelay: `${event/15}s`,
+          background: `rgb(255, 242, 0)`,
+        }
+      }
+      else {
+        return {
+          transform: `rotate(${7.2*event}deg)`,
+          animationDelay: `${event/15}s`,
+        }
+      }
+    },
+    calculateDateDifference() {
+      const start = new Date(this.startDate);
+      const end = new Date(this.endDate);
+      const differenceInTime = end.getTime() - start.getTime();
+      const differenceInDays = Math.floor(differenceInTime / (1000 * 3600 * 24));
+      this.dateDifference = differenceInDays;
     },
   },
 };
@@ -376,10 +433,6 @@ export default {
   opacity: 0;
   animation: animate 0.1s linear forwards;
 }
-.card .rating .block_temp:nth-child(-n+13){
-  background: rgb(255, 16, 16);
-  box-shadow: 0 0 15px rgb(255, 16, 16), 0 0 30px rgb(255, 16, 16);
-}
 .card .rating .block_hum{
   position: absolute;
   width: 2px;
@@ -390,11 +443,7 @@ export default {
   opacity: 0;
   animation: animate 0.1s linear forwards;
 }
-.card .rating .block_hum:nth-child(-n+23){
-  background: rgb(0, 255, 251);
-  box-shadow: 0 0 15px rgb(0, 255, 251), 0 0 30px rgb(0, 255, 251);
-}
-.card .rating .block_eco{
+.card .rating .block_soil{
   position: absolute;
   width: 2px;
   height: 13px;
@@ -403,10 +452,6 @@ export default {
   transform-origin: 50% 60px;
   opacity: 0;
   animation: animate 0.1s linear forwards;
-}
-.card .rating .block_eco:nth-child(-n+20){
-  background: rgb(255, 242, 0);
-  box-shadow: 0 0 15px rgb(255, 242, 0), 0 0 30px rgb(255, 242, 0);
 }
 @keyframes animate{
   to{
