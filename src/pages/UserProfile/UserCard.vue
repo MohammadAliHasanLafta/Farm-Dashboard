b49cd697e674427e6dc0204ab499feb8de14d967
MohammadAliHasanLafta <1380lafta@gmail.com>
2024-01-10 10:35:31 -0800

    Name added

--- src/pages/UserProfile/UserCard.vue
+++ src/pages/UserProfile/UserCard.vue
@@ -5,12 +5,9 @@
     </div>
 
     <md-card-content>
-      <h6 class="category text-gray">Front-end Developer</h6>
-      <h4 class="card-title">Mohammad Ali Hasan Lafte</h4>
+      <h6 class="category text-gray">Faculty Member</h6>
+      <h4 class="card-title">Dr. Sepahvand</h4>
       <p class="card-description">
-        Don't be scared of the truth because we need to restart the human
-        foundation in truth And I love you like Kanye loves Kanye I love Rick
-        Owens’ bed design but the back is...
       </p>
       <md-button class="md-round md-success">Follow</md-button>
     </md-card-content>
@@ -22,7 +19,7 @@ export default {
   props: {
     cardUserImage: {
       type: String,
-      default: require("@/assets/img/faces/marc.jpg"),
+      default: require("@/assets/img/faces/Dr.sepahvand.jpg"),
     },
   },
   data() {
